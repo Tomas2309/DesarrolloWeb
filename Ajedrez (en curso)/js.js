105a169d0c6154049749abd5c01188e2c9049614
Tomas Aguirre <tomasignaguirre@gmail.com>
2023-08-25 11:13:20 -0300

    Add files via upload
    Pagina web para jugar ajedrez

---
+++ Ajedrez (en curso)/js.js
@@ -0,0 +1,9 @@
+const partida_amigo=document.querySelector("#modo1");
+const partida_torneo=document.querySelector("#modo2");
+document.addEventListener('click',informacion_jugadores);
+function informacion_jugadores(e){
+    e.preventDefault();
+    partida_torneo.className="remover";
+    const texto1=document.createElement("div");
+    console.log("hjhhh")
+}
